26854c8f9c8e43fed47ab7daf36c32a859d42118
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-31 15:12:47 -0700

    photo list inital

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,10 +8,14 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   return (
     <li className="photo-list__item" key={props.id}>
-      <img src={props.imageSource}/>
-      <img src={props.profile}/>
-      <p>{props.username}</p>
-      <p>{`${props.location.city}, ${props.location.country}`}</p>
+      <img className="photo-list__image" src={props.imageSource} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.profile} />
+        <div className="photo-list__user-info">
+          <p>{props.username}</p>
+          <p className="photo-list__user-location">{`${props.location.city}, ${props.location.country}`}</p>
+        </div>
+      </div>
     </li>
   );
 };
